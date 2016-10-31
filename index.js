372966b113f1b7eb8c00c7d4d6d70bd74da101f2
cyrigues <cyrigues@gmail.com>
2016-10-31 20:38:17 +0100

    weather-webhook for nodejs

--- index.js
+++ index.js
@@ -2,54 +2,148 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var request = require("request");
 
 
-const restService = express();
-restService.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 
-restService.post('/hook', function (req, res) {
+app.post('/hook', function (req, res) {
 
-    console.log('hook request');
+        console.log('hook request');
 
-    try {
-        var speech = 'empty speech';
+        try {
+            var speech = 'empty speech';
 
-        if (req.body) {
-            var requestBody = req.body;
 
-            if (requestBody.result) {
-                speech = '';
+            var body = req.body;
 
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
+            if (typeof body == 'undefined' || body == null || body === '') {
+                console.log('Body of request is not defined or null');
+                return {};
             }
+
+            var action = body.result.action;
+            if (action !== 'yahooWeatherForecast') {
+                console.log('Action [' + body.result.action + '] is unknown... ');
+                return {};
+            } else {
+
+                var city = body.result.parameters['geo-city'];
+                console.log('Getting wether of city [' + city + ']');
+                var weatherParam = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '")';
+
+                var options = {
+                    uri: 'https://query.yahooapis.com/v1/public/yql',
+                    qs: {'q': weatherParam, 'format': 'json'}
+                };
+
+                //request(options, weatherCallback);
+                request(options, function (err, response, body) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log("Get Status response: " + response.statusCode);
+                    console.log("Get Body response: " + body);
+                    body = JSON.parse(body);
+                    var query = body.query;
+                    console.log("query: " + query);
+                    if (typeof query == 'undefined') {
+                        return {};
+                    }
+
+                    var results = query.results;
+                    if (typeof results == 'undefined') {
+                        return {};
+                    }
+
+                    var channel = results.channel;
+                    if (typeof channel == 'undefined') {
+                        return {};
+                    }
+
+                    var item = channel.item;
+                    var location = channel.location;
+                    var units = channel.units;
+                    if (typeof item == 'undefined' || typeof location == 'undefined' || typeof units == 'undefined') {
+                        return {};
+                    }
+
+                    var condition = item.condition;
+                    if (typeof condition == 'undefined') {
+                        return {};
+                    }
+                    speech = "Today in " + location.city + ": " + condition.text + ", the temperature is " + condition.temp + " " + units.temperature;
+                    console.log('result: ', speech);
+
+                    res.json({
+                        speech: speech,
+                        displayText: speech,
+                        source: 'apiai-webhook-sample'
+                    });
+                });
+            }
+
+
         }
+        catch
+            (err) {
+            console.error("Can't process request", err);
 
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
+            res.status(400).json({
+                status: {
+                    code: 400,
+                    errorType: err.message
+                }
+            });
+        }
     }
-});
+);
+function weatherCallback(err, response, body) {
+    var speech = "";
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("Get Status response: " + response.statusCode);
+    console.log("Get Body response: " + body);
+    var query = body.query;
+    console.log("query: " + query);
 
-restService.listen((process.env.PORT || 5000), function () {
+    if (typeof query == 'undefined') {
+        return {};
+    }
+
+    var results = query.results;
+    console.log("query: " + query);
+    if (typeof results == 'undefined') {
+        return {};
+    }
+
+    var channel = results.channel;
+    console.log("query: " + query);
+    if (typeof channel == 'undefined') {
+        return {};
+    }
+
+    var item = channel.item;
+    var location = channel.location;
+    var units = channel.units;
+    if (typeof item == 'undefined' || typeof location == 'undefined' || typeof units == 'undefined') {
+        return {};
+    }
+
+    var condition = item.condition;
+    if (typeof condition == 'undefined') {
+        return {};
+    }
+    speech = "Today in " + location.city + ": " + condition.text + ", the temperature is " + condition.temp + " " + units.temperature;
+    console.log('result: ', speech);
+    return speech;
+}
+
+
+app.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
 });
